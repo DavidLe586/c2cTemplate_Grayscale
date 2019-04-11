6e212026b5938008d832cb2749c21f9dc8ed612c
David Le <David.Le.586@gmail.com>
2019-04-11 22:25:45 +0000

    Was late for change

--- flask_app.py
+++ flask_app.py
@@ -7,3 +7,14 @@ def main():
 
 if __name__ == '__main__':
     app.run()
+
+
+# For web requests called via http:
+@app.route('/process_inputs', methods=['POST'])
+
+def process_inputs():
+    # Get the input data from the form into variables
+    email = request.form.get('inputEmail', '')
+    g = open('email_list.txt', 'a')  # write to the end of the file so you don't lose data.
+    g.write(email)
+    g.close
