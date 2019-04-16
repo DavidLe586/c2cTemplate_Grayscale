150e06a18a9b28c44c046245a580a530876cece5
David Le <David.Le.586@gmail.com>
2019-04-16 22:05:20 +0000

    made change for email

--- flask_app.py
+++ flask_app.py
@@ -3,11 +3,7 @@ app = Flask(__name__)
 
 @app.route('/', methods=['GET'])
 def main():
-    return render_template("index.html")
-
-if __name__ == '__main__':
-    app.run()
-
+  return render_template("index.html")
 
 # For web requests called via http:
 @app.route('/process_inputs', methods=['POST'])
@@ -15,6 +11,15 @@ if __name__ == '__main__':
 def process_inputs():
     # Get the input data from the form into variables
     email = request.form.get('inputEmail', '')
+    opinion = request.form.get('opinion','')
     g = open('email_list.txt', 'a')  # write to the end of the file so you don't lose data.
-    g.write(email)
-    g.close
+    g.write(email + "\n")
+    g.write(opinion + "\n")
+
+    g.close()
+    return render_template("index.html")
+
+
+if __name__ == '__main__':
+    app.run()
+
